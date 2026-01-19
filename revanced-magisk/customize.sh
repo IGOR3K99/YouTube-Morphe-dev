00a4b2b877845da6510fe3058484566c0bfbc95f
IGOR3K99 <55583327+IGOR3K99@users.noreply.github.com>
2026-01-19 12:16:35 +0300

    Update customize.sh

--- revanced-magisk/customize.sh
+++ revanced-magisk/customize.sh
@@ -176,6 +176,6 @@ nohup cmd package compile --reset "$PKG_NAME" >/dev/null 2>&1 &
 rm -rf "${MODPATH:?}/bin" "$MODPATH/$PKG_NAME.apk"
 
 ui_print "* Done"
-ui_print "  by sixstrings (github.com/sixstrings)"
+ui_print "  by IGOR3K99 (github.com/IGOR3K99)"
 ui_print " "
 
